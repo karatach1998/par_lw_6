cbf2f5e01866141697cc78b6eacc322281bffec8
Karatach Sergey <karatach1998@yandex.ru>
2018-05-12 11:39:09 +0300

    CPU implementation

--- main2.cu
+++ main2.cu
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 #include <argp.h>
 
 
 #define BLOCK_SIZE 16
+
+
+void mat_out(float* mat, unsigned m, unsigned n)
+{
+    for (unsigned i = 0; i < m; ++i) {
+        for (unsigned j = 0; j < n; ++j) {
+            printf("%7.3f ", mat[i * n + j]);
+        }
+        printf("\n");
+    }
+}
 
 
 void gen_mat(float* mat, unsigned m, unsigned n)
@@ -17,31 +29,47 @@ void gen_mat(float* mat, unsigned m, unsigned n)
 
 
 __global__
-void device_kernel(float* a, float* b, int m, int n)
+void device_kernel(float* a, int m, int n)
 {
     int tx = threadIdx.x;
     int ty = threadIdx.y;
     int bx = blockIdx.x;
     int by = blockIdx.y;
 
-    float tmp = a[ty * n + tx];
-    __syncthreads();
-    a[ty * n + tx] = b[tx * n + ty];
-    b[tx * n + ty] = tmp;
+    // if (tx < n && ty < m) {
+        float tmp = a[tx * n + ty];
+        __syncthreads();
+        a[ty * n + tx] = a[tx * n + ty];
+    // }
 }
 
 
 void run_using_gpu(float* mat, unsigned m, unsigned n)
 {
-    device_kernel<<<dim3(), dim(BLOCK_SIZE, BLOCK_SIZE)>>>(mat, mat, m, n);
+    float* dev;
+
+    cudaMalloc((void**)&dev, sizeof (float[m * n]));
+    cudaMemcpy(dev, mat, sizeof (float[m * n]), cudaMemcpyHostToDevice);
+
+    device_kernel<<<dim3(1, 1), dim3(BLOCK_SIZE, BLOCK_SIZE)>>>(dev, m, n);
+
+    cudaMemcpy(mat, dev, sizeof (float[m * n]), cudaMemcpyDeviceToHost);
+    cudaFree(dev);
 }
 
 
 void run_using_cpu(float* mat, unsigned m, unsigned n)
 {
-
+#pragma omp parallel for
+    for (unsigned i = 0; i < m; ++i) {
+#pragma omp parallel for
+        for (unsigned j = i; j < n; ++j) {
+            float tmp = mat[i * n + j];
+            mat[i * n + j] = mat[j * m + i];
+            mat[j * m + i] = tmp;
+        }
+    }
 }
-
 
 
 enum executor_type { DEVICE, HOST };
@@ -80,20 +108,25 @@ struct argp argp = { options, parse_arg };
 
 int main(int argc, char* argv[])
 {
-    struct config config = { 0 };
+    struct config config = { };
 
     argp_parse(&argp, argc, argv, 0, 0, &config);
 
     unsigned n = config.n;
-    float* m = (float*) malloc(sizeof float[n * n]);
+    float* m = (float*) malloc(sizeof (float[n * n]));
 
     gen_mat(m, n, n);
 
+    double start = omp_get_wtime();
     switch (config.executor)
     {
         case DEVICE: run_using_gpu(m, n, n); break;
         case HOST:   run_using_cpu(m, n, n); break;
     }
+    double stop = omp_get_wtime();
+
+    mat_out(m, n, n);
+    printf("** Execution time: %f\n", stop - start);
 
     return 0;
 }
