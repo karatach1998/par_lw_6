210f82c5c8963a6f8dbead8ac7473d9cb1c57fec
Karatach Sergey <karatach1998@yandex.ru>
2018-05-18 23:05:29 +0300

    T2: First optimized version

--- main2.cu
+++ main2.cu
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <omp.h>
 #include <argp.h>
-
-
-#define BLOCK_SIZE 16
 
 
 void mat_out(float* mat, unsigned m, unsigned n)
@@ -28,19 +26,22 @@ void gen_mat(float* mat, unsigned m, unsigned n)
 }
 
 
-__global__
-void device_kernel(float* a, int m, int n)
-{
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
+#define TILE_SIZE 16
 
-    if (tx < n && ty < m) {
-        float tmp = a[tx * n + ty];
-        __syncthreads();
-        a[ty * m + tx] = a[tx * n + ty];
-    }
+
+__global__
+void transpose_optimized(float* a, int n)
+{
+    __shared__ float tile[TILE_SIZE][TILE_SIZE];
+
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    tile[threadIdx.y][threadIdx.x] = a[y * n + x];
+
+    __syncthreads();
+
+    a[y * n + x] = tile[threadIdx.x][threadIdx.y];
 }
 
 
@@ -49,10 +50,15 @@ void run_using_gpu(float* mat, unsigned m, unsigned n)
     printf("[%u %u]\n", m, n);
     float* dev;
 
+    assert(m == n);
+    assert(m % TILE_SIZE == 0 && n % TILE_SIZE == 0);
+
     cudaMalloc((void**)&dev, sizeof (float[m * n]));
     cudaMemcpy(dev, mat, sizeof (float[m * n]), cudaMemcpyHostToDevice);
 
-    device_kernel<<<dim3(m * n / 32 * 32), dim3(32, 32)>>>(dev, m, n);
+    uint3 blocks = dim3(m / TILE_SIZE, n / TILE_SIZE);
+    uint3 threads = dim3(TILE_SIZE, TILE_SIZE);
+    transpose_optimized<<<blocks, threads>>>(dev, m, n);
 
     cudaMemcpy(mat, dev, sizeof (float[m * n]), cudaMemcpyDeviceToHost);
     cudaFree(dev);
@@ -61,9 +67,8 @@ void run_using_gpu(float* mat, unsigned m, unsigned n)
 
 void run_using_cpu(float* mat, unsigned m, unsigned n)
 {
-#pragma omp parallel for
+#pragma omp parallel for collapse(2)
     for (unsigned i = 0; i < m; ++i) {
-#pragma omp parallel for
         for (unsigned j = i + 1; j < n; ++j) {
             float tmp = mat[i * n + j];
             mat[i * n + j] = mat[j * m + i];
@@ -124,15 +129,15 @@ int main(int argc, char* argv[])
     argp_parse(&argp, argc, argv, 0, 0, &config);
 
     unsigned n = config.n;
-    float* m = (float*) malloc(sizeof (float[n * n]));
+    float* mat = (float*) malloc(sizeof (float[n * n]));
 
-    gen_mat(m, n, n);
+    gen_mat(mat, n, n);
 
     double start = omp_get_wtime();
     switch (config.executor)
     {
-        case DEVICE: run_using_gpu(m, n, n); break;
-        case HOST:   run_using_cpu(m, n, n); break;
+        case DEVICE: run_using_gpu(mat, n, n); break;
+        case HOST:   run_using_cpu(mat, n, n); break;
     }
     double stop = omp_get_wtime();
 
